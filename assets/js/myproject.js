0b9537f2a89ec2ec2da3f5f452ec823df3015919
avicienna <vcienna09@gmail.com>
2024-01-05 14:53:25 +0700

    day 5

--- assets/js/myproject.js
+++ assets/js/myproject.js
@@ -2,27 +2,12 @@ let dataBlog = [];
 let dataIcons = [];
 
 function submitBlog() { 
-   
+
     let inputProject = document.getElementById('inputTitle').value;
     let startDate = document.getElementById('startDate').value;
     let endDate = document.getElementById('endDate').value;
     let desc = document.getElementById('inputContent').value;
-  
-    // deklarasi variabel icons
-    let icon1 = document.getElementById('nodeJs');
-    let icon2 = document.getElementById('reactJs');
-    let icon3 = document.getElementById('nextJs');
-    let icon4 = document.getElementById('typescript');
-
-    // Check Icon
-    const iconsForCurrentCard = [];
-    icon1.checked === true && iconsForCurrentCard.push(nodejs);
-    icon2.checked === true && iconsForCurrentCard.push(reactjs);
-    icon3.checked === true && iconsForCurrentCard.push(nextjs);
-    icon4.checked === true && iconsForCurrentCard.push(typescript);
-
-    console.log('data');
-
+    let inputImage = document.getElementById('inputImage').files;
     // Memeriksa apakah nilai input kosong
     if (!inputProject) {
         alert('Project Name harus diisi!');
@@ -36,13 +21,65 @@ function submitBlog() {
     } else if (!desc) {
         alert('Description harus diisi!');
         return;
+    } else if (inputImage.length<=0) {
+        alert('Image harus diisi!');
+        return;
     } 
 
-    let inputImage = document.getElementById('inputImage').files;
-
-
-
     inputImage = URL.createObjectURL(inputImage[0]);
+
+    const jarak = Math.abs(new Date(endDate) - new Date(startDate));
+    
+    // Konversi jarak waktu menjadi hari, jam, menit, dan detik
+    let hari = Math.floor((jarak / (1000 * 60 * 60 * 24)));
+    let jam = Math.floor((jarak % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let menit = Math.floor((jarak % (1000 * 60 * 60)) / (1000 * 60));
+    let detik = Math.floor((jarak % (1000 * 60)) / 1000);
+    let bulan =   Math.abs(jarak / (1000 * 60 * 60 * 24 * 30));
+    bulan=   parseInt( bulan)
+    hari=hari%30;
+    
+    let durasinya="";
+    if(detik>0) {
+        durasinya += detik.toString() + " Detik, ";
+    }
+    if(menit>0) {
+        durasinya += menit.toString() + " Menit, ";
+    }
+    if(jam>0) {
+        durasinya += jam.toString() + " Jam, ";
+    }
+    if(hari>0) {
+        durasinya += hari.toString() + " Hari, ";
+    }
+    if(bulan>0) {
+        durasinya += bulan.toString() + " Bulan";
+    }
+
+ // icon checkbox
+ const nodejs = `./img/nodejs.png`;
+ const reactjs = `./img/react.png`;
+ const nextjs = `./img/nextjs.png`;
+ const typescript = `./img/typescript.png`;
+
+    // deklarasi variabel icons
+    let icon1 = document.getElementById('nodeJs');
+    let icon2 = document.getElementById('reactJs');
+    let icon3 = document.getElementById('nextJs');
+    let icon4 = document.getElementById('typescript');
+
+    // Check Icon
+    const iconsForCurrentCard = [];
+    icon1.checked === true && iconsForCurrentCard.push(nodejs);
+    icon2.checked === true && iconsForCurrentCard.push(reactjs);
+    icon3.checked === true && iconsForCurrentCard.push(nextjs);
+    icon4.checked === true && iconsForCurrentCard.push(typescript);
+
+    
+
+    console.log('data', iconsForCurrentCard);
+
+   
     
 
     const blog = {
@@ -50,7 +87,13 @@ function submitBlog() {
         startDate: startDate,
         endDate: endDate,
         desc: desc,
-        image: inputImage
+        image: inputImage,
+        bulan:bulan,
+        hari: hari,
+        jam:jam,
+        menit:menit,
+        detik:detik,
+        durasinya:durasinya
     };
 
     dataBlog.push(blog);
@@ -62,7 +105,7 @@ function submitBlog() {
 function renderBlog() {
     
     document.getElementById('contents').innerHTML = '';
-
+console.log('durasi',dataBlog [0])
     for (let i = 0; i < dataBlog.length; i++) {
 
         // Menggunakan map untuk membuat array elemen HTML untuk icon current card
@@ -74,7 +117,7 @@ function renderBlog() {
 
             <a href="project-detail.html" class="text-title">${dataBlog[i].project}</a>
 
-            <p class="text-duration">durasi : 3 bulan</p>
+            <p class="text-duration">durasi : ${dataBlog[i].durasinya} </p>
 
             <p class="text-card">${dataBlog[i].desc}</p>
             
@@ -89,6 +132,7 @@ function renderBlog() {
             <button class="btn-card">edit</button>
             <button class="btn-card">delete</button>
         </div>
-    </div>`;
+    </div>`
+    
     }
 }
