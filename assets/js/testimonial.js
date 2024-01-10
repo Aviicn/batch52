0b9a02676eea6f1e80b2061bc1c4313b88769eed
avicienna <vcienna09@gmail.com>
2024-01-11 06:52:16 +0700

    day9

--- assets/js/testimonial.js
+++ assets/js/testimonial.js
@@ -1,92 +1,64 @@
-const testimonialData = [
-  {
-    author: "-Ji Chang Wook-",
-    content: "Apa apaan ini!",
-    image: "assets/img/jcw.jpeg",
-    rating: 1,
-    
-  },
-  {
-    author: "-Kim Mingyu-",
-    content: "oke lah!",
-    image: "assets/img/mgy.jpeg",
-    rating: 3,
-  },
-  {
-    author: "-Jake-",
-    content: "The best pelayanannya!",
-    image: "assets/img/jk.jpeg",
-    rating: 4,
-    
+const ambilbackend = new Promise((resolve, reject) => {
+  const datanya = new XMLHttpRequest()
+  datanya.open('GET', 'https://api.npoint.io/5888d7eb41f2dd12de5d', true)
+  datanya.onload = () => {
+      if (datanya.status === 200) {
+          //console.log("berhasil", xhr.response)
+          resolve( JSON.parse(datanya.response))
+      } else {
+          //console.log("gagal", xhr.response)
+          reject(datanya.responseText)
+      }
+  }
+
+  datanya.onerror = () => {
+      reject("Network error!")
+      //console.log("Network error! please check your internet connection")
+  }
+  datanya.send()
+})
+
+let testimonialData =""
+
+async function allTestimonials() {
+  let testimonialHTML = ""
+   testimonialData = await ambilbackend
   
-  },
-  {
-    author: "-Kim Beom-",
-    content: "keren banget jasanya!",
-    image: "assets/img/kimbum.jpeg",
-    rating: 5,
-    
-  },
-  {
-    author: "-Cha Eun Woo-",
-    content: "kerenlah pokoknya!",
-    image: "assets/img/cew.jpeg",
-    rating: 4,
-  },
-];
+  console.log(testimonialData)
+     
+  testimonialData.forEach((item) => (
+      testimonialHTML += `
+      <div class="testimonial-container">
+      <img src="${item.image}" class="testimonial-image" /> 
+      <p class ="testimonial-text"> ${item.content}</p>
+      <p class ="testimonial-author"> ${item.author}</p>
+      <p class ="testimonial-author"> ${item.rating} <i class="fa -solid fa-star"></i></p>
+      </div>`
+  ))
 
-function allTestimonials() {
-  let testimonialHTML = "";
+  document.getElementById("testimonials").innerHTML = testimonialHTML
 
-  testimonialData.forEach(
-    (item) =>
-      (testimonialHTML += `
-        <div class="testimonial-container">
-        <img src="${item.image}" class="testimonial-img" /> 
-        <p class ="testimonial-text"> ${item.content}</p>
-        <p class ="testimonial-author"> ${item.author}</p>
-        <p class ="testimonial-author"> ${item.rating} <i class="fa -solid fa-star"></i></p>
-        </div>`)
-  );
-
-  document.getElementById("testimonials").innerHTML = testimonialHTML;
 }
 
-allTestimonials();
+allTestimonials()
 
 function filterTestimonials(rating) {
-  const testimonialsFiltered = testimonialData.filter(
-    (item) => item.rating === rating
-  );
+  const testimonialsFiltered = testimonialData.filter((item) => (
+      item.rating === rating
+  ))
 
-  let testimonialHTML = "";
-  testimonialsFiltered.forEach(
-    (item) =>
-      (testimonialHTML += `
-        <div style="width: 260px; height: 100px; display="row" class="testimonial-container">
-        <img src="${item.image}" class="testimonial-image" /> 
-        <p class ="testimonial-text"> ${item.content}</p>
-        <p class ="testimonial-author"> ${item.author}</p>
-        <p class ="testimonial-author"> ${item.rating} <i class="fa -solid fa-star"></i></p>
-        </div>`)
-  );
+  let testimonialHTML = ""
+  testimonialsFiltered.forEach((item) => (
+      testimonialHTML += `
+      <div style="width: 260px; height: 100px; display="row" class="testimonial-container">
+      <img src="${item.image}" class="testimonial-image" /> 
+      <p class ="testimonial-text"> ${item.content}</p>
+      <p class ="testimonial-author"> ${item.author}</p>
+      <p class ="testimonial-author"> ${item.rating} <i class="fa -solid fa-star"></i></p>
+      </div>`
+  ))
 
-  document.getElementById("testimonials").innerHTML = testimonialHTML;
-  console.log("data terfiter", testimonialsFiltered[0]);
+  document.getElementById("testimonials").innerHTML = testimonialHTML
+  console.log("data terfiter", testimonialsFiltered[0])
 }
 
-
-const listNav = document.getElementById("listNav");
-
-document.getElementById("hamburger").onclick = () => {
-  listNav.classList.toggle("active");
-};
-
-
-const hamburger = document.getElementById("hamburger");
-
-document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !listNav.contains(e.target)) {
-    listNav.classList.remove("active");
-  }
-});
